d048ea25ece9ca01532c832cfd02806eb5de7b3a
Martin Burga <martin.burga@udla.edu.ec>
2023-11-29 16:33:32 -0500

    Suma de matrices realizada y solucion a errores que se presentaban anteriormente,

--- Sumamatrices.c
+++ Sumamatrices.c
@@ -2,83 +2,61 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
-{
-    int dx;
-    int dy;
-    int numero;
-    int numero2;
-    int dx2;
-    int dy2;
-    int suma;
+int main(void) {
+    int dx, dy, dx2, dy2, suma = 0;
 
     printf("Inserta la dimension de su matriz en las filas\n");
     scanf("%d", &dx);
     printf("Inserta la dimension de su matriz en las columnas\n");
     scanf("%d", &dy);
-    printf(" \n");
     printf("Inserta la dimension de su segunda matriz en las filas\n");
     scanf("%d", &dx2);
     printf("Inserta la dimension de su segunda matriz en las columnas\n");
     scanf("%d", &dy2);
+
     int matrix[dx][dy];
     int matriz[dx2][dy2];
- 
-    // Se le pide al usuario las dimensiones de sus dos matrices.
 
-    if (dx == dx2 && dy == dy2)
-
-    {
-        for (int i = 0; i < dx; i++)
-        {
-            for (int j = 0; j < dy; j++)
-            {
-                numero = rand() % 100;
-                numero = rand() % (numero + 1);
+    if (dx == dx2 && dy == dy2) {
+        for (int i = 0; i < dx; i++) {
+            for (int j = 0; j < dy; j++) {
+                int numero = rand() % 100;
                 matrix[i][j] = numero;
             }
         }
 
-        for (int ix = 0; ix < dx2; ix++)
-        {
-            for (int jx = 0; jx < dy2; jx++)
-            {
-                numero2 = rand() % 100;
-                numero2 = rand() % (numero2 + 1);
-                matrix[ix][jx] = numero2;
+        for (int i = 0; i < dx2; i++) {
+            for (int j = 0; j < dy2; j++) {
+                int numero = rand() % 100;
+                matriz[i][j] = numero;
             }
         }
 
-       
-        for (int i = 0; i < dx; i++)
-        {
-            for (int j = 0; j < dy; j++)
-            {
-
+        printf("Tu primera Matriz es:\n");
+        for (int i = 0; i < dx; i++) {
+            for (int j = 0; j < dy; j++) {
                 printf("%d ", matrix[i][j]);
             }
             printf("\n");
         }
 
-        printf(" \n");
-        printf("Tu segunda Matriz es: \n");
-
-        for (int ix = 0; ix < dx2; ix++)
-        {
-            for (int jx = 0; jx < dy2; jx++)
-            {
-
-                printf("%d ", matriz[ix][jx]);
+        printf("Tu segunda Matriz es:\n");
+        for (int i = 0; i < dx2; i++) {
+            for (int j = 0; j < dy2; j++) {
+                printf("%d ", matriz[i][j]);
             }
             printf("\n");
         }
 
-        suma = matrix[dx][dy] + matriz[dx2][dy2];
-
-        printf("La suma de las matrices con numeros aleatorios es: %d\n", suma);
-    }
-    else if (dx != dx2 || dy != dy2)
-    {
+        printf("La suma de las matrices con numeros aleatorios es:\n");
+        for (int i = 0; i < dx; i++) {
+            for (int j = 0; j < dy; j++) {
+                suma = matrix[i][j] + matriz[i][j];
+                printf("%d ", suma);
+            }
+            printf("\n");
+        }
+    } else if (dx != dx2 || dy != dy2) {
         printf("La suma de matrices no se puede realizar si las dimensiones no son iguales\n");
     }
 
